0be4812b88bbc63d42b1d0f95401149db4ce8f4e
felipemimoura <felipemimoura@gmail.com>
2023-01-31 20:56:30 -0300

    feat: initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import rocketLaunch from "../../assets/rocket.svg"
+export function Header () {
+    return (
+        <div>
+            <img src={rocketLaunch} alt="Logo de um foguete decolando" />
+        </div>
+    )
+}
