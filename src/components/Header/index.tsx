e51beba8cdbee884ee8572301f3620c2b9d7ac22
felipemimoura <felipemimoura@gmail.com>
2023-02-01 08:45:36 -0300

    feat: create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,12 @@
 import rocketLaunch from "../../assets/rocket.svg"
+import styles from "./Header.module.css"
 export function Header () {
     return (
-        <div>
-            <img src={rocketLaunch} alt="Logo de um foguete decolando" />
-        </div>
+        <header className={styles.header}>
+            <img className={styles.image} src={rocketLaunch} alt="Logo de um foguete decolando" />
+            <h1 className={styles.title}>
+                to<span>do</span>
+            </h1>
+        </header>
     )
 }
