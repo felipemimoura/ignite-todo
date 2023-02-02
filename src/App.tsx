5b92a821842a220c8774b357a4955cbb9006d9fe
felipemimoura <felipemimoura@gmail.com>
2023-02-02 08:40:43 -0300

    feat: add task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import { Header } from "./components/Header";
+import { FormEvent } from "react";
 import { AiOutlinePlusCircle } from "react-icons/ai";
+
+import { Header } from "./components/Header";
+import { TaskList } from "./components/task";
+
 import "./global.css";
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
 function App() {
+  
+  const handleAddNewText = (event: FormEvent) => {};
+
   return (
     <div className="App">
       <Header />
       <div className={styles.wrapper}>
-        <form className={styles.todoForm}>
-          <input type="text" className={styles.textInput} placeholder="Adicione um a nova tarefa" />
+        <form onSubmit={handleAddNewText} className={styles.todoForm}>
+          <input
+            type="text"
+            className={styles.textInput}
+            placeholder="Adicione um a nova tarefa"
+          />
           <button type="submit">
             Criar <AiOutlinePlusCircle />
           </button>
         </form>
+        <TaskList />
       </div>
     </div>
   );
